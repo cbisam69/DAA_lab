4f9d5512f4a77780709d5adbedd2330de782e042
SHAMS AHMAD <75977026+cbisam69@users.noreply.github.com>
2022-08-10 01:21:20 +0530

    Create merge.java

---
+++ merge.java
@@ -0,0 +1,81 @@
+class Merge{
+ static void merge(int a[], int beg, int mid, int end)    
+{    
+    int i, j, k;  
+    int n1 = mid - beg + 1;    
+    int n2 = end - mid;    
+      
+    int A[]= new int[n1];
+    int B[]= new int[n2];
+      
+     
+    for (i = 0; i < n1; i++)    
+    A[i] = a[beg + i];    
+    for (j = 0; j < n2; j++)    
+    B[j] = a[mid + 1 + j];    
+      
+    i = 0;  
+    j = 0; 
+    k = beg;
+      
+    while (i < n1 && j < n2)    
+    {    
+        if(A[i] <=B[j])    
+        {    
+            a[k] = A[i];    
+            i++;    
+        }    
+        else    
+        {    
+            a[k] = B[j];    
+            j++;    
+        }    
+        k++;    
+    }    
+    while (i<n1)    
+    {    
+        a[k] = A[i];    
+        i++;    
+        k++;    
+    }    
+      
+    while (j<n2)    
+    {    
+        a[k] = B[j];    
+        j++;    
+        k++;    
+    }    
+}    
+  
+ static void mergeSort(int a[], int beg, int end)  
+{  
+    if (beg < end)   
+    {  
+        int mid = (beg + end) / 2;  
+        mergeSort(a, beg, mid);  
+        mergeSort(a, mid + 1, end);  
+        merge(a, beg, mid, end);  
+    }  
+}  
+  
+ static void printArray(int a[], int n)  
+{  
+    int l;  
+    for (l = 0; l < n; l++)  
+    System.out.println(a[l]);  
+    System.out.println("\n");  
+}  
+  
+public static void main(String[]args)  
+{  
+    int a[] = { 12, 31, 25, 8, 32, 17, 40, 42 };  
+    int n = 8;  
+    System.out.println("Before sorting array elements are -");  
+    printArray(a, n);  
+    mergeSort(a, 0, n - 1);  
+    System.out.println("After sorting array elements are - \n");  
+    printArray(a, n);  
+     
+} 
+
+}
