628453483026c9acd0fedfb61c478382189cf0f3
SHAMS AHMAD <75977026+cbisam69@users.noreply.github.com>
2022-08-11 14:43:10 +0530

    Update quicksorts.java

--- quicksorts.java
+++ quicksorts.java
@@ -62,5 +62,45 @@ class quicksorts{
     System.out.println("Sorted array: ");
     print(arr, n);
     }
-    
 }
+    /* // for labs
+     public static void main(String[] args) {
+    int n;
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter n value");
+    n = scanner.nextInt();
+    int arr[] = new int[n];
+    System.out.println("Quick Sort");
+    System.out.println("1. Best/Average Case");
+    System.out.println("2. Worst Case");
+    int ch = scanner.nextInt();
+    switch (ch) {
+    case 1:
+        Random random = new Random();
+        for (int i = 0; i < n; i++)
+        {
+        arr[i] = random.nextInt();
+        }
+        break;
+    case 2:
+        for (int i = 0; i < n; i++)
+        {
+         arr[i] = i + 1;
+        }
+        break;
+    }
+ long start = System.nanoTime();
+ quicksort(arr,0,n - 1);
+ long end = System.nanoTime();
+ System.out.println("Sorted Array");
+ for (int i = 0; i < n; i++) {
+ System.out.println(arr[i]);
+ }
+ System.out.println("Time taken: " + (end - start));
+}
+}
+
+
+    
+    
+
