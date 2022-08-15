a051d3e420e245a38fc7057829bbcf9f462a465e
SHAMS AHMAD <75977026+cbisam69@users.noreply.github.com>
2022-08-15 12:51:35 +0530

    Update floyds.java

--- floyds.java
+++ floyds.java
@@ -1,54 +1,67 @@
 import java.util.*;
-class floyds{
-    public static void main(String []args){
-        Scanner sc =new Scanner(System.in);
-        int D[][],graph[][],v;
-        int i=0;
-        int j=0;
-        int k=0;
-        System.out.println("enter the number of vertices");
-        v=sc.nextInt();
-        D= new int[v][v];
-        graph= new int[v][v];
-        System.out.println("enter the adjacency matrix");
-        for(i=0;i<v;i++)
-        {
-            for(j=0;j<v;j++)
-            {
-                graph[i][j]=sc.nextInt();
-                
-            }
-        }
-        for(i=0;i<v;i++)
-        {
-            for(j=0;j<v;j++)
-            {
-                D[i][j]=graph[i][j];
-                
-            }
-        }
-     for(k=0;k<v;k++)
-     {
-        for(i=0;i<v;i++)
-        {
-            for(j=0;j<v;j++)
-            {
-                if(D[i][k]+D[k][j]<D[i][j])
-                {
-                D[i][j]= D[i][k]+D[k][j]; 
-                }
-                
-            }
-        }
-      }    
+import java.lang.*;
+import java.io.*;
+ 
+ 
+class Main
+{   
     
-    System.out.println("Distance matrix is");
-    for(i=0;i<v;i++)
+    public static void main (String[] args)
     {
-        System.out.println(D[i][j]+" ");
-    }
-    }
-}    
-  
+      Scanner sc=new Scanner(System.in);
+      int INF = 99999;
+      int i, j, k;
+      System.out.println("enter the no. of vertex");
+      int V=sc.nextInt();
+      int graph[][] = new int[V][V];
+        System.out.println("enter the adjacency matrix");  
+        
+        for (i=0; i<V;i++)
+        {
+            for (j=0; j<V;j++)
+            {  
+                graph[i][j]=sc.nextInt();
+            
+            }
+        } 
+        int dist[][] = new int[V][V];
+        for (i = 0; i < V; i++)
+        {
+            for (j = 0; j < V; j++)
+            {
+                dist[i][j] = graph[i][j];
+            }
+        }    
+
+        for (k = 0; k < V; k++)
+        {
+            
+            for (i = 0; i < V; i++)
+            {
+                for (j = 0; j < V; j++)
+                {
+                    
+                    if (dist[i][k] + dist[k][j] < dist[i][j])
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+ 
+        
+ 
+        System.out.println("The following matrix shows the shortest "+"distances between every pair of vertices");
+        for (i=0; i<V;i++)
+        {
+            for (j=0; j<V;j++)
+            {
+                if (dist[i][j]==INF)
+                    System.out.print("INF ");
+                else
+                    System.out.print(dist[i][j]+"   ");
+            }
+            System.out.println();
+        }
     
-    
+    }
+}
+ 
