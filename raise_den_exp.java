a63c326f51ba5bb075399d2d32a2d21914efff16
SHAMS AHMAD <75977026+cbisam69@users.noreply.github.com>
2022-09-04 10:35:58 +0530

    Update raise_den_exp.java

--- raise_den_exp.java
+++ raise_den_exp.java
@@ -2,12 +2,16 @@
 // when b is not zero. Raise an exception when b is equal to zero
 
 
+// Write a Java program to read two integers a and b. Compute a/b and print, 
+// when b is not zero. Raise an exception when b is equal to zero
+
+
 import java.util.*;
 public class raise_den_exp
 { 
   public static sop(String text)
   {
-    System.out.println("text");
+    System.out.println(text);
   }  
   public static void main(String[]args)
   {
@@ -24,7 +28,7 @@ public class raise_den_exp
     }
     catch(ArithmeticException ae)
     {
-      sop(ae);
+      System.out.println(ae);
     }
   }    
       
